2c6f79ed1179d05b5f26a5864a2d71a3531ed454
syeonkoh <syeonko12@gmail.com>
2022-09-13 17:20:36 -0400

    Assignment 1

--- script.js
+++ script.js
@@ -1,43 +1,15 @@
 debugger;
-// what is debugger. I can step through this line by line
-// console.log("HI"+5);
-// alert('Hello world!');
-// cmd + / 
-// javascript does not differntiate between different kinds of numbers such as floats and integers ...
-// text
-// boolean values: true/false value without quotation
-// typeof operator explains what type it is
-// console.log(typeof 42);
 
-// let means I am about to create a variable. 
-let number_ = 42 + 10;
-// let 은 한번만 가능
+let num = 2012;
 
-//  redeclear of the variables
-number_ = number_ - 3;
-// num /= 3 same manipulate the stored values. 
-console.log(number_);
+num += 10;
 
+console.log('First variable', num);
 
-let foo = "5";
-number_ += +foo;
-console.log(typeof number_);
+let name = 'Seungyeon';
+let intro = 'My name is ';
+let str = intro + name + '.';
+console.log('Second variable', str);
 
-
-// What if compare text to number
-console.log(number_ === foo);
-
-// parseInt place string into it we convert string into number 
-// or we can add plus sign into string. it converts to number.
-
-// Dom manipulation javascript css
-// getElementsByTagName p {}
-// getElementById  #foo{}
-// getElementByClassName .bar{}
-
-// Difference between class and ID
-// ID" unique identifier, only once, class can be used multiple times
-
-//Put this on the top of this page, let output = document.getElementById('output').innerHTML = number_;
-let output = document.getElementById('output').innerHTML = number_;
-// save this variable so I can referencing whenever I need. 
+let output_first = document.getElementById('output_first').innerHTML = num;
+let output_second = document.getElementById('output_second').innerHTML = str;
