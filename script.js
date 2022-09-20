c893c4db123c7d6982a556be34c02984859eaae7
syeonkoh <syeonko12@gmail.com>
2022-09-20 17:08:32 -0400

    wk3

--- script.js
+++ script.js
@@ -1,15 +1,74 @@
-debugger;
 
-let num = 2012;
 
-num += 10;
+let groceries = ["bananas", "milk", "apple"];
 
-console.log('First variable', num);
+for (let i=0, ii=groceries.length; i<ii; i++){
+    console.log(i, groceries[i]);
+};
 
-let name = 'Seungyeon';
-let intro = 'My name is ';
-let str = intro + name + '.';
-console.log('Second variable', str);
+// instead of above for loop, we can simplify for loop like this
 
-let output_first = document.getElementById('output_first').innerHTML = num;
-let output_second = document.getElementById('output_second').innerHTML = str;
+//for (let g of groceries){
+//    console.log(g);
+//}
+
+//object: 
+//when we want to provide a greater level of detail
+let item = {
+    name: "bananas",
+    price: "3.49",
+    aisle: 1
+};
+
+//what if i want to know about only price of one item
+// item.price
+
+//for (x in item){
+//    console.log(x, item[x])
+//}
+
+// objects within arrays
+let grocery_lst = [
+    {name: 'bananas', price: 3.49, aisle:1},
+    {name: 'milk', price:2.99, aisle:4},
+    {name: 'cereals', price:7, aisle:7}
+]
+//for (let g of grocery_lst){
+//    console.log(g.name + " are " + g.price + " in aisle " + g.aisle)
+//}
+
+//functions
+function lst_item(g) {
+    
+    console.log(g.name + " are " + g.price + " in aisle " + g.aisle)
+}
+//console: lst_item(grocery_lst[0])
+
+//function different way
+//let lst_Item =function(){} 이런식
+
+
+for (let g of grocery_lst){
+    lst_item(g)
+}
+
+//Adding DOM elements
+
+let groceries_lst = [
+    {name: 'bananas', price: 3.49, aisle:1},
+    {name: 'milk', price:2.99, aisle:4},
+    {name: 'cereals', price:7, aisle:7}
+]
+
+const ul = document.getElementById('grocery-lst');
+
+function lst_items(g) {
+    let li = document.createElement('li');
+    li.innerHTML = g.name + " are " + g.price + " in aisle " + g.aisle;
+    ul.appendChild(li);
+    
+}
+
+for (let g of groceries_lst){
+    lst_items(g)
+}
