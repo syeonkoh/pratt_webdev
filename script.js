8c2af7313b477722bd865a2b5097fd5ce527c498
syeonkoh <syeonko12@gmail.com>
2022-09-20 18:41:12 -0400

    Add semicolon

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let colors = ["Red", "Blue", "Black", "Yellow", "White"];
 //console, log each item in the array
 for (let i=0, ii=colors.length; i<ii; i++){
     console.log(i, colors[i]);
-};
+}
 
 //Write each item in your array to the DOM
 const ul = document.getElementById('color_lst');
@@ -13,9 +13,10 @@ function lst_colors(color) {
     li.innerHTML = color;
     ul.appendChild(li);
     
-}
+};
+
 for (let color of colors){
-    lst_colors(color)
+    lst_colors(color);
 }
 
 //Create an object with at least 3 properties.
@@ -24,10 +25,10 @@ let users = [
     {name: 'Emily', Age: 25, Type:'B'},
     {name: 'Jin', Age: 27, Type:'C'},
     {name: 'Song', price:17, Type:'B'}
-]
+];
 
 //for...in loop, iterate through the object and log each property's key and value to the console.
 for (let x in users){
-    console.log(users[x])
+    console.log(users[x]);
 }
     
